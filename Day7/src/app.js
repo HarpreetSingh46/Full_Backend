1c96fa49186163eb54b01001d538632e3fead3c2
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-02-22 22:02:24 +0530

    Day7-updated

--- Day7/src/app.js
+++ Day7/src/app.js
@@ -1,8 +1,21 @@
 const express = require("express")
 const app = express()
+const noteModel = require("./models/notes.modle")
 
+app.use(express.json())
 
-
+app.post("/notes",async function(req,res){
+    const {title,description,age} = req.body;
+ const  note =  await  noteModel.create({
+        title,
+        description,
+        age,
+    })
+    res.status(201).json({
+        message:"note created successfully",
+        note
+    })  
+})
 
 
 module.exports= app
--- Day7/src/config/database.js
+++ Day7/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 function connectToDb(){
-     mongoose.connect("mongodb://localhost:27017")
+     mongoose.connect(process.env.MONGO_URI)
      .then(()=>{
         console.log("connecting......")
      })
