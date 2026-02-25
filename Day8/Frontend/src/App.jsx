3726ed82b9fbceb51cf8ca32e23bfb2b0bc4be37
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-02-25 21:46:15 +0530

    updated

--- Day8/Frontend/src/App.jsx
+++ Day8/Frontend/src/App.jsx
@@ -1,44 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import axios from  "axios"
-const App = () => {
-  const [notes, setnotes] = useState([])
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
-  function fetchNotes(){
+function App() {
 
-    axios.get('https://localhost:3000/api/notes')
+  const [ notes, setNotes ] = useState([])
+
+  console.log("hello Integration")
+
+  function fetchNotes() {
+    axios.get("https://cohort-2-0-m32u.onrender.com/api/notes")
+      .then(res => {
+        setNotes(res.data.notes)
+      })
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const { title, description } = e.target.elements
+
+    console.log(title.value, description.value)
+
+    axios.post("https://cohort-2-0-m32u.onrender.com/api/notes", {
+      title: title.value,
+      description: description.value
+    })
+      .then(res => {
+        console.log(res.data)
+
+        fetchNotes()
+        
+      })
+
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("https://cohort-2-0-m32u.onrender.com/api/notes/"+noteId)
     .then(res=>{
       console.log(res.data)
+      fetchNotes()
     })
   }
-  useEffect(()=>{
-    fetchNotes()
-  },[])
-  
-function HandleSubmit(e){
-  e.preventDefault()
-  const {title,description}= e.target.elements;
-  console.log(title.value,description.value)
-}
+
 
   return (
-    <div>
-      <form className='note-create-form'  onSubmit={HandleSubmit}>
-        <input name='title' type="text" placeholder='enter title' />
-        <input name='description' type="text" placeholder='enter description' id="" />
-        <button>Submit</button>
+    <>
+
+      <form className='note-create-form' onSubmit={handleSubmit}  >
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter description' />
+        <button>Create note</button>
       </form>
+
       <div className="notes">
         {
-          notes.map(note=>{
-
-      return  <div className="note">
-          <h1>{note.title}</h1>
-          <p>{note.description}</p>
-        </div>
+          notes.map(note => {
+            return <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+              <button onClick={()=>{handleDeleteNote(note._id)}} >delete</button>
+            </div>
           })
         }
       </div>
-    </div>
+    </>
   )
 }
 
