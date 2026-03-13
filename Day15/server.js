c58c747981b05afbdd836149aeccf067a082e367
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-03-13 23:04:19 +0530

    socket-io

--- Day15/server.js
+++ Day15/server.js
@@ -4,14 +4,35 @@ import { Server }  from "socket.io";
 
 
 
+
+
+
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
   console.log("new connection created")
 
-  socket.on("message")
+  socket.on("message",function(msg){
+    console.log("user fired message event")
+    console.log(msg)
+    io.emit("abc")
+  })
 });
+
+
+// socket.emit()
+// socket.broadcast().emit()
+// io.emit()
+
+
+
+
+
+
+
+
 
 httpServer.listen(3000,function(){
     console.log("server is running at port 3000")
