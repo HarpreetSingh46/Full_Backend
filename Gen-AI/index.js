e3aebff5fb550eb6e974c1f17d9b06cfc261eb79
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-03-19 22:14:25 +0530

    Gen-AI

--- Gen-AI/index.js
+++ Gen-AI/index.js
@@ -1,7 +1,23 @@
 import "dotenv/config";
 import readline from 'readline/promises';
 import {ChatMistralAI} from "@langchain/mistralai";
-import {HumanMessage} from "langchain";
+import {HumanMessage , createAgent} from "langchain";
+import { sendEmail } from "./mail.service.js";
+import { tool } from "langchain";
+import * as z from "zod";
+import { describe } from "zod/v4/core";
+const emailTool = tool(
+  sendEmail,
+  {
+    name:"sendEmail",
+    description:"use this tool to send email to anyone",
+    schema:z.object({
+      to:z.string().describe("subject of the email"),
+      html:z.string().describe("html content of the email"),
+      subject:z.string().describe("subject of the email"),
+    })
+  }
+)
 
 
 const rl = readline.createInterface({
@@ -15,14 +31,21 @@ const model = new ChatMistralAI({
   mistralApiKey: process.env.MISTRAL_API_KEY,
 });
 
+const agent =  createAgent({
+  model,
+  tools:[emailTool]
+})
+
 const messages = []
 
 while(true){
   const userInput = await rl.question("you:  ")
   messages.push(new HumanMessage(userInput))
-  const response = await model.invoke(messages)
-  messages.push(response)
-  console.log("MistralAI: ", response.text)
+  const response = await agent.invoke({ 
+     messages 
+    })
+  messages.push(response.messages[response.messages.length - 1  ])
+  console.log("MistralAI: ", response )
 }
 
 
