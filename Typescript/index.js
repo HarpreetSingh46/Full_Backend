3a1252ff6cf0ba37f5af5bc2143059d069170756
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-03-31 21:38:10 +0530

    TypeScript

---
+++ Typescript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hlo");
+console.log(1 + 1);
+const a = "world";
+const b = 5;
+console.log(a + b);
---
+++ Typescript/index.ts
@@ -0,0 +1,9 @@
+console.log("Hlo")
+console.log(1+1)
+
+
+const a = "world"
+const b = 5
+
+console.log(a+b)
+console.log(a-b)
