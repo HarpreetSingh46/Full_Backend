02e5763f40e154c209ae27897f5b0477772487ba
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-04-01 21:05:38 +0530

    LangGraph_Ai_Battle

--- langGraph-battle-ai-areana/src/services/models.ts
+++ langGraph-battle-ai-areana/src/services/models.ts
@@ -1,6 +1,7 @@
 import { ChatGoogle } from "@langchain/google";
 import config  from "../config/config.js";
 import {ChatMistralAI} from "@langchain/mistralai"
+import {ChatCohere} from "@langchain/cohere"
 export  const model  =  new ChatGoogle({
     model: "gemini-flash-latest",
     apiKey: config.GOOGLE_API_KEY
@@ -10,4 +11,9 @@ export  const model  =  new ChatGoogle({
 export  const mistralai =  new ChatMistralAI({
     model  :"mistral-medium-latest",
     apiKey : config.MISTRAL_API_KEY
+})
+
+export  const cohereModel= new ChatCohere({
+    model:"command-a--03-2025",
+    apiKey: config.COHERE_API_KEY
 })
