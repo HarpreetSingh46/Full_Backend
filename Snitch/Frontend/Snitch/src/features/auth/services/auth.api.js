ddfc5e9eeb64dcfae33c8468ee07927b10a4d8ee
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-04-10 23:27:34 +0530

    snitch

--- Snitch/Frontend/Snitch/src/features/auth/services/auth.api.js
+++ Snitch/Frontend/Snitch/src/features/auth/services/auth.api.js
@@ -1,11 +1,19 @@
-import axios from "axios"
+import axios from "axios";
 
 const authApiInstance = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "/api/auth",
     withCredentials: true,
-});
+})
 
-export async function register({ fullname, email, contact, password, isSeller }) {
-    const response = await authApiInstance.post("/register", { fullname, email, contact, password, isSeller });
-    return response.data;
+
+export async function register({ email, contact, password, fullname, isSeller }) {
+
+    const response = await authApiInstance.post("/register", {
+        email,
+        contact,
+        password,
+        fullname,
+        isSeller
+    })
+    return response.data
 }
