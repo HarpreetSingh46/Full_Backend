0c90b3ff54a9375671438656adb51d742ef6b4d1
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-04-14 22:08:22 +0530

    Snitch

--- Snitch/Backend/src/routes/product.route.js
+++ Snitch/Backend/src/routes/product.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { authenticateSeller } from "../middleware/auth.middleware.js";
 import multer from "multer";
 import { createProduct } from "../controllers/product.controller.js";
+import { createProductValidator } from "../validators/product.validator.js";
 
 const ProductRouter = express.Router();
 const upload = multer({ 
@@ -12,7 +13,7 @@ const upload = multer({
 });
 
  
-ProductRouter.post("/", authenticateSeller, upload.array("images", 7 )   , createProduct);
+ProductRouter.post("/", authenticateSeller, createProductValidator ,  upload.array("images", 7 )   , createProduct);
 
 
 export default ProductRouter
