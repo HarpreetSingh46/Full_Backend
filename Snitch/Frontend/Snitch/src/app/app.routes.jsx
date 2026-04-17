785585f13d939a0b23cd348bda8459fd9d304669
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-04-17 22:24:59 +0530

    snitch

--- Snitch/Frontend/Snitch/src/app/app.routes.jsx
+++ Snitch/Frontend/Snitch/src/app/app.routes.jsx
@@ -1,40 +1,48 @@
-
 import { createBrowserRouter } from "react-router-dom";
+
 import Register from "../features/auth/pages/Register";
 import Login from "../features/auth/pages/Login";
 import CreateProduct from "../features/products/pages/createProduct";
 import Dashboard from "../features/products/pages/Dashboard";
-import Protected from "../features/auth/component/Protected";
 
 const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <h1>Hello world</h1>
-    },
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: "/seller",
-        children: [
-            {
-                path: "/seller/products/create",
-                element: <Protected>
-                    <CreateProduct/>
-                </Protected>
-            },
-            {
-                path: "/seller/dashboard",
-                element: <Dashboard />
-            }
-        ]
-    }
-]);
+  {
+    path: "/",
+    element: <h1>Hello world</h1>,
+  },
 
+  {
+    path: "/register",
+    element: <Register />,
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+  // ✅ Seller Protected Routes
+  {
+    path: "/seller",
+    children: [
+      {
+        path: "products/create", // ✅ no /seller again
+        element: (
+       
+            <CreateProduct />
+          
+        ),
+      },
+      {
+        path: "dashboard",
+        element: (
+          
+            <Dashboard />
+          
+        ),
+      },
+    ],
+  },
+]);
 
 export default routes;
