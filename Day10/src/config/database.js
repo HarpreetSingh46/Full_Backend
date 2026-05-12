a95572548051c3f59164de193dde010d6d8a8188
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-05-12 22:43:59 +0530

    Log server startup after database connection
    Added a log message indicating server startup.

--- Day10/src/config/database.js
+++ Day10/src/config/database.js
@@ -4,6 +4,7 @@ function  connectToDb(){
     mongoose.connect('mongodb://localhost:27017/')
     .then(()=>{
         console.log("connect to the database..... \nConnected Successfully");
+        console.log("Server Starting......... ")
     })
 }
 module.exports =connectToDb
