3460dcdb0312264446650ed0d22bceb3360112e9
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-05-16 22:57:48 +0530

    Refactor connectToDb function definition

--- Day10/src/config/database.js
+++ Day10/src/config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
-function  connectToDb(){
-    
+function  connectToDb()
     mongoose.connect('mongodb://localhost:27017/')
     .then(()=>{
         console.log("connect to the database..... \nConnected Successfully");
